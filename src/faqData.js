da321788e5ce7ab03282a5ef464bdc0d2df47789
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2022-09-01 15:55:14 -0500

    Created the files I need to start

---
+++ src/faqData.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id:1,
+        question: "When will my order ship?",
+        answer: "After your payment is verified it takes up to 24 hours to process and ship your order.",
+    },
+    {
+        id:1,
+        question: "When will my order ship?",
+        answer: "After your payment is verified it takes up to 24 hours to process and ship your order.",
+    },
+    {
+        id:1,
+        question: "When will my order ship?",
+        answer: "After your payment is verified it takes up to 24 hours to process and ship your order.",
+    },
+    {
+        id:1,
+        question: "When will my order ship?",
+        answer: "After your payment is verified it takes up to 24 hours to process and ship your order.",
+    },
+    {
+        id:1,
+        question: "When will my order ship?",
+        answer: "After your payment is verified it takes up to 24 hours to process and ship your order.",
+    },
+    {
+        id:1,
+        question: "When will my order ship?",
+        answer: "After your payment is verified it takes up to 24 hours to process and ship your order.",
+    }
+]
