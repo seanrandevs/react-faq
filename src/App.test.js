6f683b5fb23dee3e120061c97d37c0918cea8976
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2022-09-05 13:55:52 -0500

    Updated with passed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Frequently Asked Questions link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Frequently Asked Questions/i);
   expect(linkElement).toBeInTheDocument();
 });
